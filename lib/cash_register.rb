9b407e8ab3a976b2667a422f9e878c5694f7f042
+ <+>
2021-10-16 21:08:54 -0500

    commit tree 4d59c6d1bbd1d50ffe7564c5e98eff2ac2c26cec

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister
+    attr_accessor :total, :discount
 
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total += (price * quantity)
+    end
+
+    def apply_discount(title, price, discount = 20)
+        @total = price -= (price * 0.2)
+    end
+end
